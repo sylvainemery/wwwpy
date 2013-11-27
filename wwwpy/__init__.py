b3e81bec2f729e70c08846fafff0901273e732b2
Sylvain Emery <github@emery.sc>
2013-11-27 23:39:03 +0100

    initial bootstrap styling

--- wwwpy/__init__.py
+++ wwwpy/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+Bootstrap(app)
 
 db = SQLAlchemy(app)
 
